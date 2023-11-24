2369ba0017f9a7254d1ed740613372f25f1223f1
ElenaBezro <lenyka_86@mail.ru>
2023-11-24 14:53:06 +0100

    implement bank transaction

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,47 @@ import properties.PropertiesDB;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 
 public class Main {
     private static final PropertiesDB properties = new PropertiesDB();
+
+    public static void bankTransaction(String url, String user, String password, double amount, int firstAccID, int secondAccID) {
+
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            connection.setAutoCommit(false);
+
+            String sqlFrom1Withdraw = "UPDATE bank_accounts SET balance = balance - ? WHERE id = ?;";
+            String sqlTo2Deposit = "UPDATE bank_accounts SET balance = balance + ? WHERE id = ?;";
+
+            try (PreparedStatement transferFrom1Withdraw = connection.prepareStatement(sqlFrom1Withdraw);
+                 PreparedStatement transferTo2Deposit = connection.prepareStatement(sqlTo2Deposit)) {
+
+                transferFrom1Withdraw.setDouble(1, amount);
+                transferFrom1Withdraw.setInt(2, firstAccID);
+
+                transferTo2Deposit.setDouble(1, amount);
+                transferTo2Deposit.setInt(2, secondAccID);
+
+                int affectedRows1 = transferFrom1Withdraw.executeUpdate();
+                int affectedRows2 = transferTo2Deposit.executeUpdate();
+
+                if (affectedRows1 > 0 && affectedRows2 > 0) {
+                    connection.commit();
+                } else {
+                    connection.rollback();
+                }
+            }
+            catch (SQLException e) {
+                connection.rollback();
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+        e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
         Connection connection = null;
@@ -23,11 +59,9 @@ public class Main {
             connection = DriverManager.getConnection(url, user, password);
             //Delete Flight using transaction
             DatabaseFlight.getInstance().deleteFlight(connection, 1);
-        }
-        catch (SQLException e) {
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             try {
                 if (connection != null || !connection.isClosed()) {
                     connection.close();
@@ -63,5 +97,27 @@ public class Main {
         Booking changedBooking = DatabaseBooking.getInstance().selectBooking(7);
         System.out.println(changedBooking);
         DatabaseBooking.getInstance().deleteBooking(2);
+
+        System.out.println("Faruk task");
+        //Exercise 1:
+        //Create a Java program using JDBC that performs multiple money transfers between multiple bank accounts within a single transaction.
+        //Implement a rollback mechanism to ensure data consistency in case of an error during any part of the transaction.
+        //
+        //To set up the database with tables and initial data, you can use the following SQL statements as an example:
+        //
+        //-- Create a bank_accounts table
+        //CREATE TABLE bank_accounts (
+        //    account_id INT AUTO_INCREMENT PRIMARY KEY,
+        //    balance DECIMAL(10, 2)
+        //);
+        //
+        //-- Insert initial account data
+        //INSERT INTO bank_accounts (account_id, balance) VALUES
+        //    (0, 1000.00),
+        //    (0, 1500.00),
+        //    (0, 800.00),
+        //    (0, 2000.00);
+        bankTransaction(url, user, password, 500.00, 1,2);
+
     }
 }
